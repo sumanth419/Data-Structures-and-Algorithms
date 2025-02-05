113d3d389aab92ec9df99362cd57be12a849612b
Sai Sumanth <104977761+sumanth419@users.noreply.github.com>
2025-02-05 20:20:57 +0530

    Update solution 6.java

--- 01. DataStructures/01. Linked List/1. Singly-Linked-List/InterviewBit Questions/06. Remove Duplicates From Sorted List/solution 6.java
+++ 01. DataStructures/01. Linked List/1. Singly-Linked-List/InterviewBit Questions/06. Remove Duplicates From Sorted List/solution 6.java
@@ -7,13 +7,16 @@
  * }
  */
 public class Solution {
-    public ListNode deleteDuplicates(ListNode A) {
+    public ListNode deleteDuplicates(ListNode A) { 
+        if (A == null || A.next == null); 
         ListNode curr = A;
-        if(A==null || A.next==null) return A;
-        while(curr.next!=null) {
-            if(curr.val == curr.next.val) curr.next = curr.next.next;
-            else curr = curr.next;
+        while (curr != null && curr.next != null) { 
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next; 
+            } else {
+                curr = curr.next; 
+            }
         }
-    return A;
+        return A;
     }
 }
